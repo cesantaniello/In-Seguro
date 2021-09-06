6a23d059cfab884a8ad9ac2505f0b8a9a4a7cec6
Administrator <admin@example.com>
2021-09-06 12:30:44 +0200

    1. Crea primer prototype

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//Constructores
+
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+function UI() {}
+
+//Llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+        min = max - 20;
+
+    const selectYear = document.querySelector('#year');
+
+    for (let i = max; i > min; i--) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);        
+    }
+}
+
+//Instanciar
+const ui = new UI();
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones();    //Lennar el select con los años
+})
